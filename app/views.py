24a0bcabab36d49f5f2081f6e0fffa7fe05974bc
sirgama <sirengogamaliel@gmail.com>
2022-05-07 13:45:04 +0300

    add alert on successful registration

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import flash, render_template, redirect, url_for
 from app import app
 from .forms import RegistrationForm, LoginForm
 
@@ -35,10 +35,13 @@ def about():
     
     return render_template('about.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     
     form= RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     
     return render_template('register.html', title='Register', form=form)
 
