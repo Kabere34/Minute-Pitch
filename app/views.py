9acf434a424e5865b45bff74afae2a6219d7e6da
sirgama <sirengogamaliel@gmail.com>
2022-05-07 06:38:43 +0300

    add dummy pitches for design purposes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
 from flask import render_template
 from app import app
 
+pitches = [
+    {
+        'author': 'Gamaliel',
+        'title' : 'Igniting Interest',
+        'content' : 'sample cont 1',
+        'category' : 'business',
+        'date_posted': 'May 6th 2022'      
+      
+    },
+    {
+        'author': 'Gamaliel',
+        'title' : 'Igniting Interest',
+        'content' : 'sample cont 1',
+        'category' : 'business',
+        'date_posted': 'May 6th 2022'      
+    
+    }
+]
+
 # Views
 @app.route('/')
 def index():
@@ -8,7 +27,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('index.html', pitches = pitches)
 
 @app.route('/about')
 def about():
