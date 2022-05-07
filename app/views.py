b0ba4c7a9fe69ac75178c7f40a5e6f804b808b11
sirgama <sirengogamaliel@gmail.com>
2022-05-07 06:22:58 +0300

    add about page

--- app/views.py
+++ app/views.py
@@ -9,3 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    
+    return render_template('about.html')
