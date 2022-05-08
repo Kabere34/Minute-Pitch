3adbd6af6db279edeffae5e4c8888e44be8c5934
sirgama <sirengogamaliel@gmail.com>
2022-05-08 23:29:04 +0300

    add user profile image update

--- pitch/forms.py
+++ pitch/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -37,8 +38,8 @@ class LoginForm(FlaskForm):
 class UpdateForm(FlaskForm):
     username = StringField('First Name', validators=[DataRequired(), Length(min=2, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    
-    submit = SubmitField('Update')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submit = SubmitField('Update Profile')
     
     def validate_username(self, username):
         if username.data != current_user.username:
--- pitch/routes.py
+++ pitch/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, request, url_for, flash, redirect
 from pitch import app, db
 from pitch.forms import RegistrationForm, LoginForm, UpdateForm
@@ -80,10 +82,26 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    
+    return picture_fn
+
 @app.route('/account', methods=['GET', 'POST'])
+@login_required
 def account():
     form = UpdateForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        
+    
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
