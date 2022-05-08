3d3b0a6f4acee11309c332c5750ae6803b0d2875
sirgama <sirengogamaliel@gmail.com>
2022-05-08 22:36:42 +0300

    add user update details model

--- pitch/forms.py
+++ pitch/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from pitch.models import User
@@ -30,3 +31,23 @@ class LoginForm(FlaskForm):
     
    
     submit = SubmitField('Login')
+    
+    
+    
+class UpdateForm(FlaskForm):
+    username = StringField('First Name', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Name is Already Taken, Please use another name!')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already taken, Please use another email!')
